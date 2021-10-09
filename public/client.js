055347c93529582ea2b5f126a7f50d2d86f42cfe
BTawaifi <52285931+BTawaifi@users.noreply.github.com>
2021-10-10 00:13:11 +0300

    General Setup

--- public/client.js
+++ public/client.js
@@ -1,8 +1,28 @@
 const btnRefresh = document.getElementById('btnRefresh');
-btnRefresh.addEventListener('click', RefreshHandler);
+btnRefresh.addEventListener('click', RefreshHandlerT);
 
+function RefreshHandlerT() {
 
+}
+
+//placeholder
 function RefreshHandler() {
-    //var mydata = JSON.parse(outer);
-    
+    var items = JSON.parse(outer);
+    itemsArray = Array.from(items);
+    let i = 0;
+    itemsArray.forEach(function (E) {
+        const row = document.createElement('option');
+        i++;
+        row.setAttribute('value', i);
+        // Build the template
+        let releaseHolder = E.children[2].innerText;
+        row.innerHTML = `
+              ${E.children[1].children[0].innerText}:${releaseHolder.replace("Released: ", "")}
+         `;
+        // Add into the cart
+        searchStatus.innerText = 'Found';
+        searchStatus.style.color = "green";
+        document.getElementById('found').appendChild(row);
+        document.getElementById('animePic').src = itemsArray[0].children[0].firstElementChild.firstElementChild.src;
+    });
 }
