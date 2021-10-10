c88a6f940a98b0bce519a72d2376f45ea262451b
BTawaifi <52285931+BTawaifi@users.noreply.github.com>
2021-10-10 18:41:56 +0300

    Added Server End Points And Frontend Fetch

--- public/client.js
+++ public/client.js
@@ -1,24 +1,18 @@
 const btnRefresh = document.querySelector('.btn-refresh');
+const inputRefresh = document.getElementById('input-refresh')
 btnRefresh.addEventListener('click', RefreshHandler);
-
-function RefreshHandlerT() {
-    fetch('./GameList.json')
-        .then(response => response.json())
-        .then(data => console.log(data))
-        .catch(error => console.log(error));
-}
 
 //placeholder
 function RefreshHandler() {
     document.querySelector('.container').innerHTML = "";
-    fetch('./GameList.json')
+    document.querySelector('.status').innerHTML = `Server Fetching 
+    <div class="status-sub">Please Wait</div>`;
+    if (inputRefresh.value==""||inputRefresh.value==undefined) {
+        inputRefresh.value=1
+    }
+    fetch(`http://localhost:5000/fetch:${inputRefresh.value}`,{method:'GET'})
         .then(response => response.json())
         .then(data => {
-            if (data == "") {
-                document.querySelector('.container').innerHTML = "Server Still Fetching";
-            }
-
-            else
                 data.forEach((mainArr) => {
                     mainArr.forEach(function (E) {
                         const link = document.createElement('a');
@@ -29,15 +23,15 @@ function RefreshHandler() {
                         div.setAttribute('class', "card");
                         div.appendChild(link);
                         link.appendChild(pic);
+                        document.querySelector('.status').innerHTML = "";
                         document.querySelector('.container').appendChild(div);
                     }
                     )
                 });
         })
         .catch(error => {
-            document.querySelector('.container').innerHTML = "Server Still Fetching";
+            document.querySelector('.status').innerHTML = "ERROR";
             console.log(error)
-
         });
 
 }
--- server.js
+++ server.js
@@ -1,60 +1,30 @@
-const puppeteer = require('puppeteer');
+const puppeteer = require("puppeteer");
 const fs = require("fs");
-const express = require('express');
+const express = require("express");
+const cors = require("cors");
 const app = express();
-app.use(express.static('public'));
-
-const mock = {
-  "outer": [
-    {
-      "pic": "https://www.skidrowreloaded.com/wp-content/uploads/2021/09/Gas-Station-Simulator-pc-free-download.jpg",
-      "link": "https://www.skidrowreloaded.com/gas-station-simulator-v1-01-38259-goldberg/"
-    },
-    {
-      "pic": "https://www.skidrowreloaded.com/wp-content/uploads/2021/10/Shelter-Manager-pc-free-download.jpg",
-      "link": "https://www.skidrowreloaded.com/shelter-manager-early-access/"
-    },
-    {
-      "pic": "https://www.skidrowreloaded.com/wp-content/uploads/2021/09/Pathfinder-Wrath-of-the-Righteous-pc-free-download.jpg",
-      "link": "https://www.skidrowreloaded.com/pathfinder-wrath-of-the-righteous-v1-0-8e-gog/"
-    },
-    {
-      "pic": "https://www.skidrowreloaded.com/wp-content/uploads/2021/10/Stonedeep-pc-free-download.jpg",
-      "link": "https://www.skidrowreloaded.com/stonedeep-plaza/"
-    },
-    {
-      "pic": "https://www.skidrowreloaded.com/wp-content/uploads/2021/10/Seduction-pc-free-download.jpg",
-      "link": "https://www.skidrowreloaded.com/seduction-darksiders/"
-    },
-    {
-      "pic": "https://www.skidrowreloaded.com/wp-content/uploads/2021/10/Castle-in-The-Clouds-DX-pc-free-download.jpg",
-      "link": "https://www.skidrowreloaded.com/castle-in-the-clouds-dx-darksiders/"
-    },
-    {
-      "pic": "https://www.skidrowreloaded.com/wp-content/uploads/2020/03/Mount-and-Blade-II-Bannerlord-pc-free-download.jpg",
-      "link": "https://www.skidrowreloaded.com/mount-and-blade-ii-bannerlord-e1-6-3-early-access/"
-    },
-    {
-      "pic": "https://www.skidrowreloaded.com/wp-content/uploads/2021/09/Life-is-Strange-True-Colors-pc-free-download.jpg",
-      "link": "https://www.skidrowreloaded.com/life-is-strange-true-colors-v1-1-192-628695-p2p/"
-    },
-    {
-      "pic": "https://www.skidrowreloaded.com/wp-content/uploads/2021/08/The-Ascent-download-pc-free.jpg",
-      "link": "https://www.skidrowreloaded.com/the-ascent-v07-10-2021-goldberg/"
-    }
-  ]
-}
+app.use(express.static("public"));
+app.use(cors({origin: "*"}));
 
 function wait(ms) {
-  return new Promise(resolve => setTimeout(() => resolve(), ms)).catch(
-    err => console.log(err)
+  return new Promise((resolve) => setTimeout(() => resolve(), ms)).catch(
+    (err) => console.log(err)
   );
+}
+
+async function writeFile(file, buffer) {
+  await fs.writeFile(file, JSON.stringify(buffer, null, 2), (err) => {
+    console.log("Operation Completed");
+    if (err) {
+      console.log(err);
+    }
+  });
 }
 
 async function lazyScrollSolver(page) {
   try {
     // Get the height of the rendered page
-    const bodyHandle = await page.$('body');
+    const bodyHandle = await page.$("body");
     const { height } = await bodyHandle.boundingBox();
     await bodyHandle.dispose();
 
@@ -62,77 +32,64 @@ async function lazyScrollSolver(page) {
     const viewportHeight = page.viewport().height;
     let viewportIncr = 0;
     while (viewportIncr + viewportHeight < height) {
-      await page.evaluate(_viewportHeight => {
+      await page.evaluate((_viewportHeight) => {
         window.scrollBy(0, _viewportHeight);
-      }, viewportHeight);
+      }, viewportHeight).catch((err)=>{
+        console.log(err);
+      });;
       await wait(20);
       viewportIncr = viewportIncr + viewportHeight;
     }
 
     // Scroll back to top
-    await page.evaluate(_ => {
+    await page.evaluate((_) => {
       window.scrollTo(0, 0);
-    });
+    }).catch((err)=>{
+      console.log(err);
+    });;
 
     // Some extra delay to let images load
-    //await wait(100);
-
+    // await wait(100);
   } catch (error) {
     console.log(error);
   }
 }
 
-(async () => {
-
-  await fs.writeFile(
-    `./public/GameList.json`,
-    '',
-    (err) => {
-      if (err) res.send(err)
-    }
-  );
-
+app.get("/fetch::num", async (req, res) =>  {
+  console.log("Starting Fetch");
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
-
-
   let outer = [];
-  for (let index = 1; index <= 5; index++) {
-    await page.goto(`https://www.skidrowreloaded.com/page/${index}`, { waitUntil: 'load' });
-    await lazyScrollSolver(page)
+  for (let index = 1; index <= req.params.num; index++) {
+    await page.goto(`https://www.skidrowreloaded.com/page/${index}`, {
+      waitUntil: "load",
+    }).catch((err)=>{
+      res.sendStatus(404, err.message);
+    });
+
+    await lazyScrollSolver(page);
 
     const evl = await page.evaluate(() => {
       let inner = [];
-      document.querySelectorAll('.post-excerpt').forEach(element => {
+      document.querySelectorAll(".post-excerpt").forEach((element) => {
         inner.push({
           pic: element.childNodes[2].firstChild.firstChild.src,
-          link: element.childNodes[2].firstChild.href
-        }
-        )
-      })
+          link: element.childNodes[2].firstChild.href,
+        });
+      });
 
+      return { inner };
+    }).catch((err)=>{
+      console.log(err);
+    });
 
-      return { inner }
-    })
-    console.log("Page" + index + " Fetched")
-    outer.push(evl.inner)
+    console.log("Page" + index + " Fetched");
+    outer.push(evl.inner);
   }
+  await browser.close().then(() => {console.log("Fetch Completed")}).catch((err)=>{
+    console.log(err);
+  });;
+  res.send(outer)
+});
 
-  //await console.log(outer);
-  await fs.appendFile(`./public/GameList.json`, JSON.stringify(outer, null, 2), (err) => {
-    console.log("Operation Completed")
-    if (err) {
-      console.log(err)
-      fs.appendFile(`./public/GameList.json`, JSON.stringify(mock, null, 2))
-    };
-  }
-  );
-
-  debugger;
-  await browser.close();
-})();
-
-
-//https://expressjs.com/en/starter/examples.html reference
-
-app.listen(5000, () => console.log("Listening on port 5000"))
+app.listen(5000, () => console.log("Listening on port 5000"));
