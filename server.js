c8f37748220cefbbaa6a61906528efa7236c045b
BTawaifi <52285931+BTawaifi@users.noreply.github.com>
2021-10-11 17:52:55 +0300

    Seperation

--- server.js
+++ server.js
@@ -2,15 +2,15 @@ const puppeteer = require("puppeteer");
 const path = require("path");
 const express = require("express");
 const cors = require("cors");
-const compression = require('compression')
+const compression = require("compression");
 //const fs = require("fs");
 
 const PORT = process.env.PORT || 5000;
 
 const app = express();
 app.use(express.static(path.join(__dirname, "public")));
-app.use(cors({origin: '*'})); //Not needed
-app.use(compression())
+app.use(cors({ origin: "*" })); //Not needed
+app.use(compression());
 
 function wait(ms) {
   return new Promise((resolve) => setTimeout(() => resolve(), ms)).catch(
@@ -39,21 +39,25 @@ async function lazyScrollSolver(page) {
     const viewportHeight = page.viewport().height;
     let viewportIncr = 0;
     while (viewportIncr + viewportHeight < height) {
-      await page.evaluate((_viewportHeight) => {
-        window.scrollBy(0, _viewportHeight);
-      }, viewportHeight).catch((err)=>{
-        console.log(err);
-      });
+      await page
+        .evaluate((_viewportHeight) => {
+          window.scrollBy(0, _viewportHeight);
+        }, viewportHeight)
+        .catch((err) => {
+          console.log(err);
+        });
       await wait(20);
       viewportIncr = viewportIncr + viewportHeight;
     }
 
     // Scroll back to top
-    await page.evaluate((_) => {
-      window.scrollTo(0, 0);
-    }).catch((err)=>{
-      console.log(err);
-    });
+    await page
+      .evaluate((_) => {
+        window.scrollTo(0, 0);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
 
     // Some extra delay to let images load
     // await wait(100);
@@ -62,52 +66,65 @@ async function lazyScrollSolver(page) {
   }
 }
 
-app.get("/fetch::num", async (req, res) =>  {
+async function fetchpage(outerArray, page, link, req,res) {
+  for (let index = 1; index <= req.params.num; index++) {
+    await page
+      .goto(`${link}${index}`, {
+        waitUntil: "load",
+      })
+      .catch((err) => {
+        res.sendStatus(404, err.message);
+      });
+    await lazyScrollSolver(page);
+    const evl = await page
+      .evaluate(() => {
+        let inner = [];
+        document.querySelectorAll(".post-excerpt").forEach((element) => {
+          inner.push({
+            pic: element.childNodes[2].firstChild.firstChild.src,
+            link: element.childNodes[2].firstChild.href,
+          });
+        });
+
+        return { inner };
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+    await console.log("Page" + index + " Fetched");
+    outerArray.push(evl.inner);
+  }
+}
+
+
+app.get("/fetch::num", async (req, res) => {
   console.log("Starting Fetch");
   const browser = await puppeteer.launch({
-    args: [
-      '--no-sandbox',
-      '--disable-setuid-sandbox',
-    ],
+    args: ["--no-sandbox", "--disable-setuid-sandbox"],
   }); //Consider changing this if it fails on local
-  
-  try{
-  const page = await browser.newPage();
-  let outer = [];
-  for (let index = 1; index <= req.params.num; index++) {
-    await page.goto(`https://www.skidrowreloaded.com/page/${index}`, {
-      waitUntil: "load",
-    }).catch((err)=>{
-      res.sendStatus(404, err.message);
-    });
-    await lazyScrollSolver(page);
-    const evl = await page.evaluate(() => {
-      let inner = [];
-      document.querySelectorAll(".post-excerpt").forEach((element) => {
-        inner.push({
-          pic: element.childNodes[2].firstChild.firstChild.src,
-          link: element.childNodes[2].firstChild.href,
-        });
-      });
 
-      return { inner };
-    }).catch((err)=>{
-      console.log(err);
-    });
-    await console.log("Page" + index + " Fetched");
-    outer.push(evl.inner);
+  try {
+    const page = await browser.newPage();
+    let outer = [];
+    fetchpage(outer,page,"https://www.skidrowreloaded.com/page/",req).then(res.send(outer))
+    .catch((err) => {console.log(err);});
+    
+  } catch (e) {
+    console.log(e);
+  } finally {
+    browser.close()
+      .then(() => {
+        console.log("Fetch Completed");
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   }
-  res.send(outer)
-} catch (e) {console.log(e);
-} finally {
-  await browser.close().then(() => {
-    console.log("Fetch Completed")
-  }
-  ).catch((err)=>{
-    console.log(err);
-  });
-}
 });
+
+app.get("/404",(req,res)=>{
+  console.log("NOT FOUND")
+})
 
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
